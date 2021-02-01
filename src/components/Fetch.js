d1bd161ed2fd595bfcd789a4eb54149e5f19b79b
smartsandeep1994 <44244839+smartsandeep1994@users.noreply.github.com>
2021-02-02 01:01:29 +0530

    Update Fetch.js

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import axios from 'axios';
 
 export default class Fetch extends React.Component{
-    constructor(props){
-      super(props);
-      this.state = {
-          content:[],
-      }
-    }
-    componentDidMount(){
-        const UserUrl = `${this.props.url}`;
-        axios.get(UserUrl).
-            then(result => {
-            console.log(result.data.data);
-                this.setState({ content: result.data.data });
-                // this.setState({ postLength: result.data.data.length });
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-        }
     render(){
       return(
         <section className="row row-flex">
-          {this.props.children(this.state.content)}
+          Demo 
         </section>
       )
     }
